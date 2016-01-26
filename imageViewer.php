6dbf03bc323b48da2aa6cd66784ce528a22af87a
kz <yoshikime2@gmail.com>
2016-01-26 11:41:56 +0900

    imageViewer

---
+++ imageViewer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="euc-kr">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<title>이미지 뷰어</title>
+
+	<style>
+		body {
+			margin:0 0 0 0;
+		}
+		#container {
+			width: 800px;
+			height: 600px; 
+			overflow: hidden;
+
+		}
+	
+	</style>
+</head>
+<body>
+	<div id="container">
+		<img src ="http://static.inven.co.kr/image_2011/tos/dataninfo/map/map_4.jpg" alt="이미지 뷰어"  id="imagename" class="zooom" style="position:relative"/>
+	</div>
+<script src="imgPop.js"></script>
+</body>
+
+</html>
---
+++ imgPop.js
@@ -0,0 +1,150 @@
+
+$(function(){
+	initializer();
+	//scrollZoom();
+	scrollZoom();
+
+	$('img').draggable(
+		{
+	    distance: 3,
+	    revert: false,
+	    scroll: false,
+	    drag: getSize,
+	    containment: [wrapperOffset.left - bgConwrapperX,
+	        wrapperOffset.top - bgConwrapperY,
+	        wrapperOffset.left,
+	        wrapperOffset.top]
+	   	});
+
+	//$("img").draggable();
+	getCurLoc();
+	
+})
+
+var img;
+var currentMouseX = null;currentMouseY = null; lPosition = null; tPosition = null; bgWidth = null; bgHeight = null; 
+var curL = null; curT = null;
+var wrapperOffset = $("#container").offset();
+var bgConwrapperX, bgConwrapperY;
+var container = $("#container");
+var bgConwrapperX = 0; 
+var bgConwrapperY = 0;
+var mTerritoryX = 0; 
+var mTerritoryY = 0;
+var conWidth = container.css("width", bgWidth);
+var conHeight = container.css("height", bgHeight);
+//드래그시 이미지가 창밖으로 나가지 않게 하기위해 draggable 오브젝트의 containment 값을 드래그시마다 지정해준다.
+function getSize(){
+	if (bgWidth <= container.width()){
+		bgConwrapperX = 0;
+	}
+	else if(bgWidth > container.width()){
+		bgConwrapperX = bgWidth-container.width();
+	}
+
+
+	if (bgHeight <= container.height()){
+		bgConwrapperY = 0;
+	}
+	else if(bgHeight > container.height()){
+		bgConwrapperY = bgHeight-container.height();
+	}
+	img.data('uiDraggable').containment[0] = wrapperOffset.left - bgConwrapperX;
+	img.data('uiDraggable').containment[1] = wrapperOffset.top - bgConwrapperY;
+}
+//이미지 사이즈를 변수에 지정하고 드래그를 활성화 시킨다. 
+function initializer() {
+	img = $("img");
+	bgWidth = img.width(); 
+	bgHeight = img.height();
+
+	if(bgWidth > 800 && bgHeight > 600) {
+	
+		$("img").css("cursor", "move");
+	}	
+}
+//마우스좌표의 위치를 구한다. 
+function getCurLoc() {
+	
+		$("img").on("mousemove", function(e) {
+			currentMouseX = e.pageX; 
+			currentMouseY = e.pageY;
+			curL = parseInt(img.css("left").replace('px',''));
+			curT = parseInt(img.css("top").replace('px', ''));		
+
+			//이미지위치를 기준으로 마우스 좌표를 설정 
+			lPosition = Math.floor(bgWidth+curL-currentMouseX); 
+			tPosition = Math.floor(bgHeight+curT-currentMouseY); 
+			//이미지위 마우스영역에 따른 계산 
+			if(currentMouseX < container.width()*0.5) {
+				mTerritoryX = -currentMouseX*0.1-10;				
+			}
+			else if(currentMouseX > container.width()*0.5) {
+				mTerritoryX = 0;
+			}
+			if(currentMouseY < container.height()*0.5) {
+				mTerritoryY = -currentMouseY*0.1-10
+			}	
+			else if(currentMouseY > container.height()*0.5) {
+				mTerritoryY = 0;
+			}	
+						
+		});
+
+}
+
+
+//스크롤시 줌 인 아웃 
+function scrollZoom() {
+		
+		$("img").on('wheel', function(e) {
+		
+			var delta = e.originalEvent.deltaY;
+			curL = parseInt(img.css("left").replace('px',''));
+			curT = parseInt(img.css("top").replace('px', ''));
+			var ratioW = bgWidth*0.1;
+			var ratioH = bgHeight*0.1;				
+			var mouseX = ratioW-lPosition*0.1; 
+			var mouseY = ratioH-tPosition*0.1; 			
+
+			if (delta > 0) {			
+				if (bgWidth > 800) {					
+						img.css("width",(bgWidth = bgWidth - ratioW)+"px");
+						img.css("height",(bgHeight = bgHeight- ratioH)+"px");
+
+						if (curT < 0 || curL < 0) {
+							img.css("left", (curL += mouseX-mTerritoryX)+"px");
+							img.css("top", (curT += mouseY-mTerritoryY)+"px");							
+						}
+						if (bgWidth < 850) {
+							img.position({
+							"at": "left top",
+							"of": $("#container"),
+							"collision": "fit"
+							});	
+
+						}
+
+
+
+						//줌 아웃시 이미지의 위피를 항상 left top 으로 고정 		
+								
+										
+				}
+
+			}
+			else {
+				if(bgWidth < 3000) {	
+						img.css("width", (bgWidth = bgWidth + ratioW)+"px");
+						img.css("height", (bgHeight = bgHeight + ratioH)+"px");
+						img.css("left", (curL -= mouseX+mTerritoryX)+"px");
+						img.css("top", (curT -= mouseY+mTerritoryY)+"px");				
+				}
+			}
+		});
+
+		
+}
+
+function gcd(a, b) { return (!b) ? a : gcd(b,a%b);}
+
