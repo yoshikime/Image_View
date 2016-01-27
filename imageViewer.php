363816b3493d3121d7ddf5170ec430a02d48836a
kz <yoshikime2@gmail.com>
2016-01-27 11:46:11 +0900

    ㅇㅇㅁㄴㅇㅁㄴㅇ

--- imageViewer.php
+++ imageViewer.php
@@ -1,7 +1,6 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-	<!--232323-->
+<head>	
 	<meta charset="utf-8">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
@@ -21,7 +20,7 @@
 	</style>
 </head>
 <body>
-	<h2>이것은 테스트 입니다.테스트 버전의 테스트</h2>
+	<h2>이것은 종결자입니다.</h2>
 
 	<div id="container">
 		<img src ="http://static.inven.co.kr/image_2011/tos/dataninfo/map/map_4.jpg" alt="이미지"  id="imagename" class="zooom" style="position:relative"/>
