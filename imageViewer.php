b24de91679e85507415220786e8198a971ef9fc3
yoshikime <yoshikime2@gmail.com>
2016-01-28 14:28:58 +0900

    ENG

--- imageViewer.php
+++ imageViewer.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-	<title>이미지 보기</title>
+	<title>Image Scroll Zoom</title>
 
 	<style>
 		body {
@@ -20,7 +20,7 @@
 	</style>
 </head>
 <body>
-	<h2>이것은 종결자입니다.</h2>
+	<h2>IMAGE SCROLL ZOOM</h2>
 
 	<div id="container">
 		<img src ="http://static.inven.co.kr/image_2011/tos/dataninfo/map/map_4.jpg" alt="이미지"  id="imagename" class="zooom" style="position:relative"/>
