8eb857fa6f0b08aa06f930da6db78f0d384b745d
yoshikime <yoshikime2@gmail.com>
2016-02-01 15:35:49 +0900

    이미지 위치 조정중

--- imgPop.js
+++ imgPop.js
@@ -20,12 +20,8 @@ $(function(){
 	$('img').dblclick(function(){
 		imgcomp('layer1'); 
 	}); 	
-	$('img').on('mouseenter', function(){
-		layerShow('#layer1');
-		$(this).on('mousemove', function() {			
-			layerInit('#layer1');	
-		})
-		
+	$('img').on('mousemove', function() {			
+			layerInit('#layer1');		
 	})
 	$('img').on('mouseleave', function() {
 		layerHide('#layer1');
@@ -46,7 +42,7 @@ var mTerritoryX = 0;
 var mTerritoryY = 0;
 var conWidth = container.css("width", bgWidth);
 var conHeight = container.css("height", bgHeight);
-var mode, html;
+var mode, html, mouseX,mouseY;
 //드래그시 이미지가 창밖으로 나가지 않게 하기위해 draggable 오브젝트의 container 값을 드래그시마다 지정해준다.
 function getSize(){
 	if (bgWidth <= container.width()){
@@ -89,6 +85,10 @@ function getCurLoc() {
 			//이미지위치를 기준으로 마우스 좌표를 설정 
 			lPosition = Math.floor(bgWidth+curL-currentMouseX); 
 			tPosition = Math.floor(bgHeight+curT-currentMouseY); 
+			//0.0 를 기준으로 이미지 마우스 좌표 설정.
+			zlPosition = Math.floor(curL-currentMouseX); 
+			ztPosition = Math.floor(curT-currentMouseY);  
+
 			//이미지위 마우스영역에 따른 계산 
 			if(currentMouseX < container.width()*0.5) {
 				mTerritoryX = -currentMouseX*0.1-10;				
@@ -118,8 +118,8 @@ function scrollZoom() {
 			curT = parseInt(img.css("top").replace('px', ''));
 			var ratioW = bgWidth*0.1;
 			var ratioH = bgHeight*0.1;				
-			var mouseX = ratioW-lPosition*0.1; 
-			var mouseY = ratioH-tPosition*0.1; 			
+			mouseX = ratioW-lPosition*0.1; 
+			mouseY = ratioH-tPosition*0.1; 			
 
 			if (delta > 0) {			
 				if (bgWidth >850) {					
@@ -158,13 +158,9 @@ function imgcomp(title) {
 	mode = true; 
 	html = "<div id='"+title+"'style='display:none;'>test</div>";
 	$("body").append(html); 
+	//layerShow("#"+title); 
 }
 
-function layerShow(title) {
-	if (mode == true) {
-		$(title).show(); 
-	}
-}
 
 function layerHide(title){
 	if (mode == true) {
@@ -172,10 +168,21 @@ function layerHide(title){
 	}
 }
 
-function layerInit(title) {	
+function layerInit(title) {
+		getCurLoc();	
+		var style = {
+			left: currentMouseX+10,
+			top: currentMouseY+10, 
+			display:"block", 
+			backgroundimage: "url("+$(document).find('img').attr('src')+")" 
+		};
+		//console.log(mouseX, mouseY); 
 		$(title).css({
-			"left": currentMouseX,
-			"top": currentMouseY
+			"left": currentMouseX+10,
+			"top": currentMouseY+10,
+			"display": "block",
+			"background-image": "url("+$(document).find('img').attr('src')+")",
+			"background-position": "left "+Math.floor(zlPosition)+"px "+"top "+Math.floor(ztPosition)+"px"
 		});	
 }
 
